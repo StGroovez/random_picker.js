42ab703f3d4a32cc6b8a25794a1f566c9bacee8a
Abdo <78060631+StGroovez@users.noreply.github.com>
2021-02-21 01:59:11 -0600

    gelou
    Nomas puse mi nombre, apenas estoy descubriendo Github jejeje.
    Le puedo agregar cambios al style? :)
    Salu2

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-const alumns = ["Sofia Cárdenas", "Jorge Medrano", "Mariangela Alejandrina Pérez Lugo", "Fabián Conejo", "Shir Sahagún", "Julian Barrios ", "Angel Antonio", "Lucia Auyón", "Andru Vargas", "Fernando Hernandez Baez", "Arturo Camargo"];
+const alumns = ["Sofia Cárdenas", "Jorge Medrano", "Mariangela Alejandrina Pérez Lugo", "Fabián Conejo", "Shir Sahagún", "Julian Barrios ", "Angel Antonio", "Lucia Auyón", "Andru Vargas", "Fernando Hernandez Baez", "Arturo Camargo", "Abdo Rodríguez"];
 
 const randomPicker = (array) => {
     let randNum = Math.floor(Math.random() * array.length);
